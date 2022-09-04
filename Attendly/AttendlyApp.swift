3284c1eba27b80f5a550abd877488cf4cf202b49
Raniaalageel <rania.kh.ala@gmail.com>
2022-09-04 22:13:37 +0300

    Initial Commit

---
+++ Attendly/AttendlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendlyApp.swift
+//  Attendly
+//
+//  Created by Rania Alageel on 08/02/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AttendlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Attendly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Attendly
+//
+//  Created by Rania Alageel on 08/02/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

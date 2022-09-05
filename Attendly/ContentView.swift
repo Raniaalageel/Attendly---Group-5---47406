64ff63519e200dee26914958d2796462b0fff24b
Raniaalageel <rania.kh.ala@gmail.com>
2022-09-05 21:12:16 +0300

    Rania

--- Attendly/ContentView.swift
+++ Attendly/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
   @State var idStudent=""
     var body: some View {
-       // Text("Hello, Rania is here")
+       // Text("Hello, Rania is here#########")
            // .padding()
         
      /*  ZStack{

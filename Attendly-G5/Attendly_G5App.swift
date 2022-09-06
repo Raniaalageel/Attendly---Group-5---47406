45f8d559026db62540ee344544cdb6f52fdb385b
Raniaalageel <rania.kh.ala@gmail.com>
2022-09-06 19:49:35 +0300

    Initial Commit

---
+++ Attendly-G5/Attendly_G5App.swift
@@ -0,0 +1,17 @@
+//
+//  Attendly_G5App.swift
+//  Attendly-G5
+//
+//  Created by Rania Alageel on 10/02/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Attendly_G5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

79664d390b75f6db9f11c072f163c08e1d0c23e4
Raniaalageel <rania.kh.ala@gmail.com>
2022-09-06 20:22:19 +0300

    hi

--- Attendly-G5/Attendly_G5App.swift
+++ Attendly-G5/Attendly_G5App.swift
@@ -5,10 +5,23 @@
 //  Created by Rania Alageel on 10/02/1444 AH.
 //
 
-import SwiftUI
 
+import SwiftUI
+import Firebase
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 @main
 struct Attendly_G5App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
--- Attendly-G5/ContentView.swift
+++ Attendly-G5/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FirebaseAnalyticsSwift
 
 struct ContentView: View {
     var body: some View {
